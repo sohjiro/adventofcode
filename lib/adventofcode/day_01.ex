4604b1d1b2b27c81da5b69cbce6438300590ed78
sohjiro <felipe@makingdevs.com>
2016-10-03 22:35:19 -0500

    refactor

--- lib/adventofcode/day_01.ex
+++ lib/adventofcode/day_01.ex
@@ -3,32 +3,24 @@ defmodule Adventofcode.Day01 do
   def enter_basement(instructions) do
     instructions
     |> to_char_list
-    |> follow_instructions(0, 0)
+    |> follow_instructions(0, 0, :basement)
   end
 
-  defp follow_instructions(_directions, index, -1), do: index
+  defp follow_instructions(_directions, index, -1, :basement), do: index
+  defp follow_instructions([], _index, floor, _atom), do: floor
 
-  defp follow_instructions([40 | directions], index, floor) do
-    follow_instructions(directions, index + 1, floor + 1)
+  defp follow_instructions([40 | directions], index, floor, atom) do
+    follow_instructions(directions, index + 1, floor + 1, atom)
   end
 
-  defp follow_instructions([41 | directions], index, floor) do
-    follow_instructions(directions, index + 1, floor - 1)
+  defp follow_instructions([41 | directions], index, floor, atom) do
+    follow_instructions(directions, index + 1, floor - 1, atom)
   end
 
   def calculate_floor(instructions) do
     instructions
     |> to_char_list
-    |> count
-  end
-
-  defp count(directions) do
-    Enum.reduce(directions, 0, fn(direction, acc) ->
-      case direction do
-        40 -> acc + 1
-        41 -> acc - 1
-      end
-    end)
+    |> follow_instructions(0, 0, :total)
   end
 
 end
