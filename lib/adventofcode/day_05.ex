7a40e8e895c72d9b9c10d9917ab019bd2fa9e6ea
sohjiro <felipe@makingdevs.com>
2016-10-12 23:30:26 -0500

    adding test

--- lib/adventofcode/day_05.ex
+++ lib/adventofcode/day_05.ex
@@ -1,12 +1,21 @@
 defmodule Adventofcode.Day05 do
-  @vowels 'aeiou'
+  @vowels ["a", "e", "i", "o", "u"]
+  @banned ["ab", "cd", "pq", "xy"]
 
   def nice_string?(string) do
-    string
-    |> to_char_list
-    |> vowels_count
+    string_split = String.split(string, "")
+
+    {vowels_count(string_split), twice_in_a_row(string_split), banned(string_split)}
   end
 
   defp vowels_count(list), do: list |> Enum.count(fn(l) -> l in @vowels end)
+
+  defp twice_in_a_row([]), do: false
+  defp twice_in_a_row([x, x | _rest]), do: true
+  defp twice_in_a_row([_x, _y | rest]), do: twice_in_a_row(rest)
+
+  defp banned(_list) do
+    false
+  end
 
 end
--- test/adventofcode/day_05_test.exs
+++ test/adventofcode/day_05_test.exs
@@ -2,9 +2,19 @@ defmodule Adventofcode.Day05Test do
   use ExUnit.Case
   doctest Adventofcode.Day05
 
-  test "the string 'aeiouaeiouaeiou' should be a nice string" do
-    result = Adventofcode.Day05.nice_string?("aeiouaeiouaeiou")
-    assert result > 2
+  test "the string should contain at least three vowels" do
+    {vowels, _twice, _banned} = Adventofcode.Day05.nice_string?("aeiouaeiouaeiou")
+    assert vowels > 2
+  end
+
+  test "the string should contain at least one letter that appears twice in a row" do
+    {_vowels, twice, _banned} = Adventofcode.Day05.nice_string?("aabbccdd")
+    assert twice
+  end
+
+  test "the string should not contain the strings ab, cd, pq, or xy" do
+    {_vowels, _twice, banned} = Adventofcode.Day05.nice_string?("haegwjzuvuyypxyu")
+    refute banned
   end
 
 end
