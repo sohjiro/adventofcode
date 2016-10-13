eb6a453c8e77a3315fe240fed44e92c3b797cbcd
sohjiro <felipe@makingdevs.com>
2016-10-12 22:17:28 -0500

    refactoring

--- lib/adventofcode/day_04.ex
+++ lib/adventofcode/day_04.ex
@@ -1,7 +1,9 @@
 defmodule Adventofcode.Day04 do
 
-  def find_number(data) do
-    find_hash(data, 1)
+  def find_number(data, number \\ 1) do
+    data <> to_string(number)
+    |> md5
+    |> validate(data, number)
   end
 
   defp md5(data) do
@@ -9,13 +11,7 @@ defmodule Adventofcode.Day04 do
     |> Base.encode16
   end
 
-  defp find_hash(data, number) do
-    data <> to_string(number)
-    |> md5
-    |> validate(data, number)
-  end
-
   defp validate("00000" <> rest, _data, number), do: {number, "00000#{rest}"}
-  defp validate(_, data, number), do: find_hash(data, number + 1)
+  defp validate(_, data, number), do: find_number(data, number + 1)
 
 end
