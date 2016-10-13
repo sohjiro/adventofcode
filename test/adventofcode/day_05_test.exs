bbd3b2f73c3927960c648f9b7962182cf431a33d
sohjiro <felipe@makingdevs.com>
2016-10-12 22:47:55 -0500

    adding test for day 05

---
+++ test/adventofcode/day_05_test.exs
@@ -0,0 +1,10 @@
+defmodule Adventofcode.Day05Test do
+  use ExUnit.Case
+  doctest Adventofcode.Day05
+
+  test "the string 'aeiouaeiouaeiou' should be a nice string" do
+    result = Adventofcode.Day05.nice_string?("aeiouaeiouaeiou")
+    assert result
+  end
+
+end
