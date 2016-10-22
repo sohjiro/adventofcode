06d79c6ceedbfcd06edb1f6aa9cd5a4e97bd81a0
sohjiro <felipe@makingdevs.com>
2016-10-22 00:09:54 -0500

    changing for regular expresions

--- lib/adventofcode/day_05.ex
+++ lib/adventofcode/day_05.ex
@@ -11,63 +11,22 @@ defmodule Adventofcode.Day05 do
   def new_nice_string(strings) do
     strings
     |> String.split("\n")
-    |> map_tuple([])
-    |> apply_rules([])
+    |> apply_new_rules([])
   end
 
-  defp map_tuple([], conversion), do: conversion
-  defp map_tuple([string | rest], conversion) do
-    charlist = to_char_list(string)
-    twice = combine_twice_letters(charlist, [])
-    thrice = combine_three_letters(charlist, [])
+  defp apply_new_rules([], results), do: results
+  defp apply_new_rules([string | rest], results) do
+    separate_by_one_letter = Regex.run(~r/([a-z])((?!\1).)\1/, string)
+    repeat = Regex.run(~r/([a-z]{2,3}).*\1/, string)
 
-    map_tuple(rest, [{string, twice, thrice} | conversion])
-  end
-
-  defp combine_twice_letters([_], list), do: list
-  defp combine_twice_letters([a, b | rest], list), do: combine_twice_letters([b | rest], [{a, b} | list])
-
-  defp combine_three_letters([_, _], list), do: list
-  defp combine_three_letters([a, b, c | rest], list), do: combine_three_letters([b, c | rest], [{a, b, c} | list])
-
-  defp apply_rules([], results), do: results
-  defp apply_rules([{string, list, thrice} | rest], results) do
-    result = case {twice?(list), overlaps?(thrice)} do
-      {0, _} -> :naughty
-      {_, true} -> :naughty
-      {_, false} -> :nice
+    result = case {repeat, separate_by_one_letter} do
+      {nil, nil} -> :naughty
+      {_, nil} -> :naughty
+      {nil, _} -> :naughty
+      {_, _} -> :nice
     end
-    apply_rules(rest, [{string, result} | results])
-  end
 
-  defp overlaps?(words) do
-    case map_thrice(words, %{}) do
-      %{overlaps: _x, repeats: _} -> true
-      %{overlaps: _x} -> true
-      %{repeats: _} -> false
-      _ -> true
-    end
-  end
-
-  defp map_thrice([], counted), do: counted
-  defp map_thrice([{a, a, a} | rest], counted) do
-    map_thrice(rest, Map.update(counted, :overlaps, 1, &(&1 + 1)))
-  end
-  defp map_thrice([{a, _b, a} | rest], counted) do
-    map_thrice(rest, Map.update(counted, :repeats, 1, &(&1 + 1)))
-  end
-  defp map_thrice([_data | rest], counted), do: map_thrice(rest, counted)
-
-  defp twice?(words) do
-    words
-    |> map_twice(%{})
-    |> Enum.filter(fn({_k, v}) -> v > 1 end)
-    |> length
-  end
-
-  defp map_twice([], counted), do: counted
-  defp map_twice([{a, b} | rest], counted) do
-    map_twice(rest, Map.update(counted, [a,b], 1, &(&1 + 1)))
+    apply_new_rules(rest, [{string, result} | results])
   end
 
   def count_nice_strings(strings) do
--- test/adventofcode/day_05_test.exs
+++ test/adventofcode/day_05_test.exs
@@ -2,32 +2,6 @@ defmodule Adventofcode.Day05Test do
   use ExUnit.Case
   doctest Adventofcode.Day05
 
-  test "the string 'ugknbfddgicrmopn' is nice" do
-    result = Adventofcode.Day05.count_nice_strings("ugknbfddgicrmopn")
-    assert result == 1
-  end
-
-  test "the string should not contain the strings ab, cd, pq, or xy" do
-    result = Adventofcode.Day05.count_nice_strings("haegwjzuvuyypxyu")
-    assert result == 0
-  end
-
-  test "the string 'jchzalrnumimnmhp' should be naughty" do
-    result = Adventofcode.Day05.count_nice_strings("jchzalrnumimnmhp")
-    assert result == 0
-  end
-
-  test "the string 'dvszwmarrgswjxmb' should be naughty" do
-    result = Adventofcode.Day05.count_nice_strings("dvszwmarrgswjxmb")
-    assert result == 0
-  end
-
-  test "given a list of strings should return the number of nice strings" do
-    strings = "ugknbfddgicrmopn\nhaegwjzuvuyypxyu\njchzalrnumimnmhp\ndvszwmarrgswjxmb"
-    result = Adventofcode.Day05.count_nice_strings(strings)
-    assert result == 1
-  end
-
   test "The string 'qjhvhtzxzqqjkmpb' should be nice" do
     [{string, result}] = Adventofcode.Day05.new_nice_string("qjhvhtzxzqqjkmpb")
 
@@ -57,9 +31,9 @@ defmodule Adventofcode.Day05Test do
   end
 
   test "given a list of strings should return the number of new nice strings" do
-    strings = "qjhvhtzxzqqjkmpb\nxxyxx\nuurcxstgmygtbstg\nieodomkazucvgmuy"
-    result = Adventofcode.Day05.count_new_nice_strings(strings)
-    assert result == 2
+    strings = "rxexcbwhiywwwwnu"
+    [{string, result}] = Adventofcode.Day05.new_nice_string(strings)
+    assert result == :nice
   end
 
 end
