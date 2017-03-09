40ace04b72891165d1a6fae1c3f431def91e4f43
sohjiro <felipe@makingdevs.com>
2017-03-09 11:23:35 -0600

    passing test, for last coordinates

--- lib/adventofcode/seventeen/day_01.ex
+++ lib/adventofcode/seventeen/day_01.ex
@@ -1,5 +1,12 @@
 defmodule Adventofcode.Seventeen.Day01 do
 
+  def calculate_coordinates(instructions) do
+    instructions
+    |> String.split(", ")
+    |> Enum.map(&convert_into_coordinate/1)
+    |> calculate_final_coordinate
+  end
+
   def convert_into_coordinate(direction) do
     direction
     |> to_charlist
@@ -8,7 +15,15 @@ defmodule Adventofcode.Seventeen.Day01 do
 
   defp map_direction([?R | blocks]), do: {to_int(blocks), 0}
   defp map_direction([?L | blocks]), do: {0, to_int(blocks)}
-
   defp to_int(char), do: char |> to_string |> String.to_integer
+
+  defp calculate_final_coordinate(coordinates) do
+    Enum.reduce(coordinates, {0, 0}, fn({x, y}, acc) ->
+      case acc do
+        {0, 0} -> {x, y}
+        {new_x, new_y} -> {x + new_x, y + new_y}
+      end
+    end)
+  end
 
 end
--- test/adventofcode/seventeen/day_01_test.exs
+++ test/adventofcode/seventeen/day_01_test.exs
@@ -11,9 +11,9 @@ defmodule Adventofcode.Seventeen.Day01Test do
     assert result == {0, 2}
   end
 
-  # test "Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away" do
-  #   blocks = Adventofcode.Seventeen.Day01.calculate_blocks "R2, L3"
-  #   assert blocks == 5
-  # end
+  test "Given an R2, L3 direction should convert into a coordinate" do
+    result = Adventofcode.Seventeen.Day01.calculate_coordinates "R2, L3"
+    assert result == {2, 3}
+  end
 
 end
