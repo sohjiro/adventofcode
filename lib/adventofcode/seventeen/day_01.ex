5b2955c47ed8f960da905f13ef34933dd8db4714
sohjiro <felipe@makingdevs.com>
2017-03-08 23:21:48 -0600

    converting left and right directions

--- lib/adventofcode/seventeen/day_01.ex
+++ lib/adventofcode/seventeen/day_01.ex
@@ -1,8 +1,14 @@
 defmodule Adventofcode.Seventeen.Day01 do
 
   def convert_into_coordinate(direction) do
-    [direction, blocks | _rest] = String.split(direction, ~r{\d+}, include_captures: true)
-    {String.to_integer(blocks), 0}
+    direction
+    |> to_charlist
+    |> map_direction
   end
+
+  defp map_direction([?R | blocks]), do: {to_int(blocks), 0}
+  defp map_direction([?L | blocks]), do: {0, to_int(blocks)}
+
+  defp to_int(char), do: char |> to_string |> String.to_integer
 
 end
--- test/adventofcode/seventeen/day_01_test.exs
+++ test/adventofcode/seventeen/day_01_test.exs
@@ -6,6 +6,11 @@ defmodule Adventofcode.Seventeen.Day01Test do
     assert result == {2, 0}
   end
 
+  test "Given an L2 direction should convert into a coordinate" do
+    result = Adventofcode.Seventeen.Day01.convert_into_coordinate "L2"
+    assert result == {0, 2}
+  end
+
   # test "Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away" do
   #   blocks = Adventofcode.Seventeen.Day01.calculate_blocks "R2, L3"
   #   assert blocks == 5
